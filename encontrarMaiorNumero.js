cf9562d0e9b72b97f31928425e52902213965c73
Breno Rippel <39842293+BrenoRippel@users.noreply.github.com>
2024-06-03 23:51:01 -0300

    criada função para encontrar o maior número em um array

---
+++ encontrarMaiorNumero.js
@@ -0,0 +1,11 @@
+function encontrarMaiorNumero(numeros) {
+    let maior = numeros[0];
+
+    for (let i = 1; i < numeros.length; i++) {
+        if (numeros[i] > maior) {
+            maior = numeros[i];
+        }
+    }
+
+    return maior;
+}
